5e968ebca0788dee72fc9cd49a1d0dd95be0b389
PragatiVerma18 <itispragativerma@gmail.com>
2021-07-16 23:46:55 +0530

    Feat (Admin) - Added list view for accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+admin.site.site_url = "/"
+
+
+@admin.register(User)
+class AccountsAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "role", "verified")
+    list_editable = ("verified",)
+    search_fields = ("username", "email")
+    list_filter = (
+        "role",
+        "verified",
+    )
