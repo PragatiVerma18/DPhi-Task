af6f8fdf883fa510cf25a680af4d68ae8c354891
PragatiVerma18 <itispragativerma@gmail.com>
2021-07-17 15:08:41 +0530

    Feat (Deploy): Added Gunicorn and Django Heroku

--- nursery/settings/common.py
+++ nursery/settings/common.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 import environ
 import os
 from os.path import abspath, dirname, exists, join
@@ -145,3 +146,6 @@ SIMPLE_JWT = {
 
 MEDIA_URL = "/mediafiles/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
